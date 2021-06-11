b58514eeed2202602a77d9022eb4959dedad5f51
Meow <65875137+Astroide@users.noreply.github.com>
2021-06-11 00:49:04 +0000

    Update to make it work on repl.it

--- run-online/db.js
+++ run-online/db.js
@@ -1,21 +1,23 @@
 const ReplitDatabase = require('@replit/database');
 class Database {
-    constructor() {
-        /** @type {ReplitDatabase} */
-        this.db = new ReplitDatabase();
-        this.contents = {};
-        this.db.get('main', { raw: true }).then(content => {
-            this.contents = JSON.parse(content);
-            setInterval(() => {
-                this.db.set('main', JSON.stringify(this.contents));
-            }, 15000);
-        });
-    }
-    getKey(key) {
-        return this.contents[key];
-    }
-    setKey(key, value) {
-        return this.contents[key] = value;
-    }
+  constructor() {
+    /** @type {ReplitDatabase} */
+    this.db = new ReplitDatabase();
+    this.contents = {};
+    this.db.get('main').then(content => {
+      console.log(content);
+      this.contents = JSON.parse(decodeURIComponent(content));
+      console.log(this.contents);
+      setInterval(() => {
+        this.db.set('main', encodeURIComponent(JSON.stringify(this.contents)));
+      }, 15000);
+    });
+  }
+  getKey(key) {
+    return this.contents[key];
+  }
+  setKey(key, value) {
+    return this.contents[key] = value;
+  }
 }
 module.exports = Database;
